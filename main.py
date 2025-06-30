e757cf5f539abd8f90bb0a8501b5fb499eb35aaf
FlabbyBawls <flabbybawls.game@gmail.com>
2025-06-30 11:36:48 +0200

    Use pygame groups. Asteroids CH2 L8

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ def main():
     dt = 0
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
@@ -19,9 +23,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)    
+        updatable.update(dt)    
         screen.fill("black")
-        player.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
+        
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
