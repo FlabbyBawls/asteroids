f51f7713f6320b700f86ae672ed6851557a5b721
FlabbyBawls <flabbybawls.game@gmail.com>
2025-06-30 10:42:03 +0200

    Player movement. Asteroids CH2 L7

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 from constants import *
-from player import *
+from player import Player
 
 def main():
     pygame.init()
@@ -18,7 +18,9 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill
+
+        player.update(dt)    
+        screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
